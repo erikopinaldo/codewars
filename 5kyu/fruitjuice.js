0c2c8cffa75405bfb1cfb88d79657b3ccd0161a5
Erik Opinaldo <erikopinaldo@gmail.com>
2022-06-07 22:16:59 -0400

    fruitjuuice.js - stored total as a property

--- 5kyu/fruitjuice.js
+++ 5kyu/fruitjuice.js
@@ -2,31 +2,30 @@
 // https://www.codewars.com/kata/5264603df227072e6500006d/train/javascript
 
 function Jar() {
-    this.volume = {}
-   }
-   
-   Jar.prototype.add = function(amount, type) {
-     if (this.volume[type]) this.volume[type] += amount
-     else this.volume[type] = amount
-   };
-   
-   Jar.prototype.pourOut = function(amount) {
-     let percent = 1 - (amount / this.getTotalAmount())
-     
-     for (let fruit in this.volume) {
-       this.volume[fruit] = this.volume[fruit] * percent
-     }
-   };
-   
-   Jar.prototype.getTotalAmount = function() {
-     let total = 0
-     for (let fruit in this.volume) {
-       total += this.volume[fruit]
-     }
-     return total
-   };
-   
-   Jar.prototype.getConcentration = function(type) {
-     if (this.volume[type]) return this.volume[type] / this.getTotalAmount()
-     else return 0
-   }
+  this.volume = {}
+  this.total = 0
+}
+
+Jar.prototype.add = function(amount, type) {
+  if (this.volume[type]) this.volume[type] += amount
+  else this.volume[type] = amount
+  this.total += amount
+};
+
+Jar.prototype.pourOut = function(amount) {
+  let percent = 1 - (amount / this.getTotalAmount())
+  
+  for (let fruit in this.volume) {
+    this.volume[fruit] = this.volume[fruit] * percent
+  }
+  this.total -= amount
+};
+
+Jar.prototype.getTotalAmount = function() {
+  return this.total
+};
+
+Jar.prototype.getConcentration = function(type) {
+  if (this.volume[type]) return this.volume[type] / this.total
+  else return 0
+}
